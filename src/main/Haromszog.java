4d556202983d6ee7393b9a9ac9645df398f0257e
Lukács Alexandra (SZF_2023_2025) <lukacs.alexandra@diak.szamalk-szalezi.hu>
2024-05-08 11:12:23 +0200

    kerulet

---
+++ src/main/Haromszog.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Haromszog {
+    private int aOldal;
+    private int bOldal;
+    private int cOldal;
+    
+    public Haromszog(int a, int b, int c){
+        this.aOldal = a;
+        this.bOldal = b;
+        this.cOldal = c;
+    }
+    
+    public void keruletSzamolo(){
+        
+    }
+    
+    
+}
